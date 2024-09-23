6aa3984da40e8fa7e9764163b512057be0681dc2
Raysayhey <rlotmar@hotmail.com>
2024-09-23 11:58:34 +0200

    delete: db prod

--- src/database/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-
-export const libsqlClient = createClient({
-  url:  "libsql://d-riffs-test-block-lab.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjY3MzkwOTYsImlkIjoiZTBhYTRlOGQtNjZjOS00OTE0LWJlN2EtNGM5MDNkMWNhZTQ4In0.l14RKm5e1Oqgn_n1TKJZ04LrbWcU3CNyNtwaqmH0gixcN9MrOkvdElxnyHehbhPWYJnVAIfMS9ytGu-_YO41CA",
-});
-
-export const db = drizzle(libsqlClient);
